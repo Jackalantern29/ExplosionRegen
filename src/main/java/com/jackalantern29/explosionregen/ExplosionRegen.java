ca5f531364a4e42a8ccd198e0e831be2bb65ad4b
Jackalantern29 <mikalmck29@gmail.com>
2020-08-08 04:08:34 -0400

    Updated saving files onDisable.

--- src/main/java/com/jackalantern29/explosionregen/ExplosionRegen.java
+++ src/main/java/com/jackalantern29/explosionregen/ExplosionRegen.java
@@ -8,7 +8,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.jackalantern29.explosionregen.api.ERExplosion;
 import com.jackalantern29.explosionregen.api.ERExplosionMap;
-import com.jackalantern29.explosionregen.api.ERSettings;
+import com.jackalantern29.explosionregen.api.ExplosionRegenSettings;
 import com.jackalantern29.explosionregen.api.ExplosionSettings;
 import com.jackalantern29.explosionregen.commands.CommandRExplode;
 import com.jackalantern29.explosionregen.commands.CommandRSettings;
@@ -20,12 +20,12 @@ public class ExplosionRegen extends JavaPlugin implements Listener {
 
 	private static ExplosionRegen instance;
 	private static ERExplosionMap explosions;
-	private static ERSettings settings;
+	private static ExplosionRegenSettings settings;
 	public static UUID author = UUID.fromString("76763b6e-4804-4b7e-bfbd-5d87c72e7843");
 	public void onEnable() {
 		instance = this;
 		explosions = new ERExplosionMap();
-		settings = new ERSettings();
+		settings = new ExplosionRegenSettings();
 		EntityExplodeListener listener = new EntityExplodeListener();
 		getServer().getPluginManager().registerEvents(listener, this);
 		try {
@@ -46,7 +46,7 @@ public class ExplosionRegen extends JavaPlugin implements Listener {
 	
 	public void onDisable() {
 		for(ExplosionSettings settings : ExplosionSettings.getRegisteredSettings()) {
-			settings.save();
+			settings.saveAsFile();
 		}
 		for(ERExplosion explosions : explosions.getExplosions()) {
 			explosions.regenerateAll();
@@ -58,7 +58,7 @@ public class ExplosionRegen extends JavaPlugin implements Listener {
 	public static ERExplosionMap getExplosionMap() {
 		return explosions;
 	}
-	public static ERSettings getSettings() {
+	public static ExplosionRegenSettings getSettings() {
 		return settings;
 	}
 	public static UUID getAuthor() {
