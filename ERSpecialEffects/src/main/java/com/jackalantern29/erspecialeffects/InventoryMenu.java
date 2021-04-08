c9061c2e9957977d62fb472c1ff81cc6e62f248a
Jackalantern29 <mikalmck29@gmail.com>
2021-04-08 11:36:36 -0400

    Initial commit

---
+++ ERSpecialEffects/src/main/java/com/jackalantern29/erspecialeffects/InventoryMenu.java
@@ -0,0 +1,235 @@
+package com.jackalantern29.erspecialeffects;
+
+import com.jackalantern29.explosionregen.api.ExplosionParticle;
+import com.jackalantern29.explosionregen.api.ExplosionSettings;
+import com.jackalantern29.explosionregen.api.ProfileSettings;
+import com.jackalantern29.explosionregen.api.enums.ExplosionPhase;
+import com.jackalantern29.explosionregen.api.inventory.ItemBuilder;
+import com.jackalantern29.explosionregen.api.inventory.PageMenu;
+import com.jackalantern29.explosionregen.api.inventory.SettingsMenu;
+import com.jackalantern29.explosionregen.api.inventory.SlotElement;
+import org.apache.commons.lang.WordUtils;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.*;
+
+public class InventoryMenu {
+    private static final HashMap<ProfileSettings, InventoryMenu> MAP = new HashMap<>();
+    private final SettingsMenu menu;
+    private ExplosionPhase phase = ExplosionPhase.BLOCK_REGENERATING;
+
+    private InventoryMenu(ProfileSettings profile) {
+
+        ItemStack closeItem = new ItemBuilder(Material.BARRIER).setDisplayName("§c§lClose Menu").build();
+        this.menu = new SettingsMenu("§2Select Explosion", 5);
+        menu.setUpdate("menu", () -> {
+            menu.clear();
+            for(ExplosionSettings settings : ExplosionSettings.getRegisteredSettings()) {
+                if(profile == null || profile.hasPermission(settings)) {
+                    SettingsMenu typeMenu = new SettingsMenu("§2Select Option", 5);
+                    PageMenu particleMenu = new PageMenu("§2Select Particle", 54);
+                    PageMenu presetMenu = new PageMenu("§2Select Preset", 54);
+                    PageMenu soundMenu = new PageMenu("§2Select Sound", 54);
+                    List<PageMenu> pageSet = new ArrayList<>();
+                    pageSet.add(particleMenu);
+                    pageSet.add(presetMenu);
+                    pageSet.add(soundMenu);
+/*                    private final String[] setupRows = {
+                            "qwertiyu0",
+                            "ppppppppd",
+                            "ppppppppz",
+                            "ppppppppn",
+                            "ppppppppm",
+                            "ppppppppv",
+                    };*/
+
+                    SpecialEffects effects;
+                    if(profile != null) {
+                        effects = (SpecialEffects)profile.getPlugin(settings, "SpecialEffects").toObject();
+                    } else {
+                        effects = (SpecialEffects)settings.getPlugin("SpecialEffects").toObject();
+                    }
+                    menu.addItem(new SlotElement(new ItemBuilder(settings.getDisplayItem()).setDisplayName(settings.getDisplayName()).build(), data -> {
+                        typeMenu.sendInventory(data.getWhoClicked());
+                        return true;
+                    }));
+                    typeMenu.setItem(1, new SlotElement(new ItemBuilder(Material.NETHER_STAR).setDisplayName("§a§lParticles").build(), data -> {
+                        particleMenu.sendInventory(data.getWhoClicked());
+                        return true;
+                    }));
+                    typeMenu.setItem(2, new SlotElement(new ItemBuilder(Material.NOTE_BLOCK).setDisplayName("§a§lSounds").build(), data -> {
+                        soundMenu.sendInventory(data.getWhoClicked());
+                        return true;
+                    }));
+                    typeMenu.setItem(4, new SlotElement(closeItem, data -> {
+                        menu.sendInventory(data.getWhoClicked());
+                        return true;
+                    }));
+
+                    for(PageMenu pageMenu : pageSet) {
+                        pageMenu.setNextPageItem(new ItemBuilder(Material.LIME_STAINED_GLASS_PANE).setDisplayName("§aNext Page").build());
+                        pageMenu.setPrevPageItem(new ItemBuilder(Material.LIME_STAINED_GLASS_PANE).setDisplayName("§aBack Page").build());
+
+                        pageMenu.setUpdate("preset", () -> {
+                            for(SettingsMenu setMenu : pageMenu.getPages()) {
+                                setMenu.setItem(0, new SlotElement(new ItemBuilder(Material.LIME_DYE).setDisplayName("§aBlock Regenerating").setGlow(phase == ExplosionPhase.BLOCK_REGENERATING).build(), data -> {
+                                    phase = ExplosionPhase.BLOCK_REGENERATING;
+                                    pageMenu.update("preset");
+                                    return true;
+                                }));
+                                setMenu.setItem(1, new SlotElement(new ItemBuilder(Material.LIGHT_BLUE_DYE).setDisplayName("§bOn Block Regen").setGlow(phase == ExplosionPhase.ON_BLOCK_REGEN).build(), data -> {
+                                    phase = ExplosionPhase.ON_BLOCK_REGEN;
+                                    pageMenu.update("preset");
+                                    return true;
+                                }));
+                                setMenu.setItem(2, new SlotElement(new ItemBuilder(Material.PURPLE_DYE).setDisplayName("§5On Explode").setGlow(phase == ExplosionPhase.ON_EXPLODE).build(), data -> {
+                                    phase = ExplosionPhase.ON_EXPLODE;
+                                    pageMenu.update("preset");
+                                    return true;
+                                }));
+                                setMenu.setItem(3, new SlotElement(new ItemBuilder(Material.PINK_DYE).setDisplayName("§dExplosion Finished Regen").setGlow(phase == ExplosionPhase.EXPLOSION_FINISHED_REGEN).build(), data -> {
+                                    phase = ExplosionPhase.EXPLOSION_FINISHED_REGEN;
+                                    pageMenu.update("preset");
+                                    return true;
+                                }));
+                                if(particleMenu.hasPage(setMenu)) {
+                                    setMenu.setItem(4, new SlotElement(new ItemBuilder(Material.COMPASS).setDisplayName("§aToggle Particle").build(), data -> {
+                                        effects.getParticleSettings(ParticleType.VANILLA).getParticles(phase).get(0).setCanDisplay(!effects.getParticleSettings(ParticleType.VANILLA).getParticles(phase).get(0).getCanDisplay());
+                                        pageMenu.update("preset");
+                                        return true;
+                                    }));
+                                    setMenu.setItem(5, new SlotElement(new ItemBuilder(Material.CLOCK).setDisplayName("§aSwitch Particle Type").setLine(0, "§7Current Type: §6Vanilla").build(), data -> {
+                                        effects.setParticleType(ParticleType.PRESET);
+                                        presetMenu.sendInventory(data.getWhoClicked());
+                                        return true;
+                                    }));
+                                } else if(soundMenu.hasPage(setMenu)) {
+                                    setMenu.setItem(4, new SlotElement(new ItemBuilder(Material.COMPASS).setDisplayName("§aToggle Sound").build(), data -> {
+                                        effects.setAllowSound(phase, !effects.getAllowSound(phase));
+                                        pageMenu.update("preset");
+                                        return true;
+                                    }));
+                                    setMenu.setItem(5, new SlotElement(new ItemBuilder(Material.BLACK_STAINED_GLASS_PANE).setDisplayName(" ").build(), data -> true));
+                                }
+                                setMenu.setItem(6, new SlotElement(pageMenu.getPrevPageItem(), data -> true));
+                                setMenu.setItem(7, new SlotElement(pageMenu.getNextPageItem(), data -> true));
+                                setMenu.setItem(8, new SlotElement(closeItem, data -> {
+                                    typeMenu.sendInventory(data.getWhoClicked());
+                                    return true;
+                                }));
+                            }
+                        });
+                    }
+
+                    particleMenu.setUpdate("particles", () -> {
+                        particleMenu.clear();
+                        particleMenu.update("preset");
+                        List<ExplosionParticle> particleList = Arrays.asList(ExplosionParticle.getParticles());
+                        particleList.sort(Comparator.comparing(ExplosionParticle::toString));
+                        for(ExplosionParticle particle : particleList) {
+                            if(profile != null && !profile.getPlayer().hasPermission("explosionregen.command.rsettings.particles." + particle.toString().toLowerCase()))
+                                continue;
+                            String displayName = WordUtils.capitalize(particle.toString().toLowerCase().replace("_", " "));
+                            ItemStack item = new ItemBuilder(Material.GRAY_DYE).setDisplayName("§7" + displayName).build();
+                            for(ExplosionPhase phaseI : ExplosionPhase.values()) {
+                                ExplosionParticle phaseParticle = effects.getParticleSettings(ParticleType.VANILLA).getParticles(phaseI).get(0).getParticle();
+                                if(particle == phaseParticle) {
+                                    switch(phaseI) {
+                                        case BLOCK_REGENERATING:
+                                            item = new ItemBuilder(Material.LIME_DYE).setDisplayName("§a" + displayName).build();
+                                            break;
+                                        case ON_BLOCK_REGEN:
+                                            item = new ItemBuilder(Material.LIGHT_BLUE_DYE).setDisplayName("§b" + displayName).build();
+                                            break;
+                                        case ON_EXPLODE:
+                                            item = new ItemBuilder(Material.PURPLE_DYE).setDisplayName("§5" + displayName).build();
+                                            break;
+                                        case EXPLOSION_FINISHED_REGEN:
+                                            item = new ItemBuilder(Material.PINK_DYE).setDisplayName("§d" + displayName).build();
+                                            break;
+                                    }
+                                    break;
+                                }
+                            }
+                            particleMenu.addItem(new SlotElement(item, data -> {
+                                effects.getParticleSettings(ParticleType.VANILLA).getParticles(phase).get(0).setParticle(particle);
+                                particleMenu.update("particles");
+                                return true;
+                            }));
+                        }
+                    });
+                    soundMenu.setUpdate("sounds", () -> {
+                        soundMenu.clear();
+                        soundMenu.update("phaseSelect");
+                        for(Sound sound : Sound.values()) {
+                            if(profile != null && !profile.getPlayer().hasPermission("explosionregen.command.rsettings.sounds." + sound.name().toLowerCase()))
+                                continue;
+                            String displayName = WordUtils.capitalize(sound.name().toLowerCase().replace("_", " "));
+                            ItemStack item = new ItemBuilder(Material.MUSIC_DISC_STAL).setDisplayName("§7" + displayName).build();
+
+                            switch(sound.name().split("_")[0]) {
+                                case "AMBIENT":
+                                    item = new ItemBuilder(Material.MUSIC_DISC_13).setDisplayName("§7" + displayName).build();
+                                    break;
+                                case "BLOCK":
+                                    item = new ItemBuilder(Material.MUSIC_DISC_BLOCKS).setDisplayName("§7" + displayName).build();
+                                    break;
+                                case "ENCHANT":
+                                    item = new ItemBuilder(Material.MUSIC_DISC_CHIRP).setDisplayName("§7" + displayName).build();
+                                    break;
+                                case "ENTITY":
+                                    item = new ItemBuilder(Material.MUSIC_DISC_CAT).setDisplayName("§7" + displayName).build();
+                                    break;
+                                case "ITEM":
+                                    item = new ItemBuilder(Material.MUSIC_DISC_FAR).setDisplayName("§7" + displayName).build();
+                                    break;
+                                case "EVENT":
+                                    item = new ItemBuilder(Material.MUSIC_DISC_WAIT).setDisplayName("§7" + displayName).build();
+                                    break;
+                                case "MUSIC":
+                                    item = new ItemBuilder(Material.MUSIC_DISC_STRAD).setDisplayName("§7" + displayName).build();
+                                    break;
+                                case "PARTICLE":
+                                    item = new ItemBuilder(Material.MUSIC_DISC_MELLOHI).setDisplayName("§7" + displayName).build();
+                                    break;
+                                case "UI":
+                                    item = new ItemBuilder(Material.MUSIC_DISC_11).setDisplayName("§7" + displayName).build();
+                                    break;
+                                case "WEATHER":
+                                    item = new ItemBuilder(Material.MUSIC_DISC_MALL).setDisplayName("§7" + displayName).build();
+                                    break;
+                            }
+
+                            for(ExplosionPhase phaseI : ExplosionPhase.values()) {
+                                Sound phaseSound = effects.getSoundSettings().getSound(phaseI).getSound();
+                                if(sound == phaseSound) {
+                                    item = new ItemBuilder(Material.NOTE_BLOCK).setDisplayName("§b" + displayName).build();
+                                    break;
+                                }
+                            }
+                            soundMenu.addItem(new SlotElement(item, data -> {
+                                SoundData soundData = effects.getSoundSettings().getSound(phase);
+                                soundData.setSound(sound);
+                                effects.getSoundSettings().setSound(phase, soundData);
+                                soundMenu.update("sounds");
+                                return true;
+                            }));
+                        }
+                    });
+                }
+            }
+        });
+        MAP.put(profile, this);
+    }
+
+    public static void sendMenu(Player player, ProfileSettings profile) {
+        if(MAP.containsKey(profile))
+            MAP.get(profile).menu.sendInventory(player);
+        else
+            new InventoryMenu(profile).menu.sendInventory(player);
+
+    }
+}
---
+++ ExplosionRegen/src/main/java/com/jackalantern29/explosionregen/api/inventory/PageMenu.java
@@ -0,0 +1,139 @@
+package com.jackalantern29.explosionregen.api.inventory;
+
+import com.jackalantern29.explosionregen.ExplosionRegen;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class PageMenu {
+    private String title;
+    private int slots;
+    private HashMap<Integer, SettingsMenu> pages = new HashMap<>();
+    private HashMap<String, DynamicUpdate> map = new HashMap<>();
+
+    private ItemStack nextPageItem = null;
+    private ItemStack prevPageItem = null;
+
+    public PageMenu(String title, int slots) {
+        this.title = title;
+        this.slots = slots;
+        Bukkit.getPluginManager().registerEvents(new ClickListen(), ExplosionRegen.getInstance());
+        pages.put(0, new SettingsMenu(this.title, this.slots));
+    }
+
+    public SettingsMenu getPage(int page) {
+        return pages.get(page);
+    }
+
+    public void addPage() {
+        pages.put(pages.size(), new SettingsMenu(getTitle(), getSlots()));
+    }
+
+    public boolean hasPage(SettingsMenu menu) {
+        return getPages().contains(menu);
+    }
+
+    public Collection<SettingsMenu> getPages() {
+        return pages.values();
+    }
+
+    public void setNextPageItem(ItemStack item) {
+        this.nextPageItem = item;
+    }
+
+    public ItemStack getNextPageItem() {
+        return nextPageItem;
+    }
+
+    public void setPrevPageItem(ItemStack item) {
+        this.prevPageItem = item;
+    }
+
+    public ItemStack getPrevPageItem() {
+        return prevPageItem;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getSlots() {
+        return slots;
+    }
+
+    public void clear() {
+        for(SettingsMenu menu : getPages()) {
+            menu.clear();
+        }
+    }
+
+    public void addItem(SlotElement element) {
+        int count = 0;
+        for(SettingsMenu menu : getPages()) {
+            int slot = menu.getInventory().firstEmpty();
+            if(slot != -1) {
+                menu.addItem(element);
+                return;
+            }
+            count++;
+        }
+        addPage();
+        update("preset");
+    }
+
+    public void sendInventory(HumanEntity player) {
+        getPage(0).sendInventory(player);
+        if(!map.isEmpty())
+            for(DynamicUpdate update : map.values())
+                update.update();
+    }
+
+    public void update(String id) {
+        if(map.containsKey(id)) {
+            map.get(id).update();
+        }
+    }
+
+    public void setUpdate(String id, DynamicUpdate update) {
+        map.put(id, update);
+    }
+
+    public interface DynamicUpdate {
+        public void update();
+    }
+
+    public class ClickListen implements Listener {
+        @EventHandler
+        public void onClick(InventoryClickEvent event) {
+            for (int i = 0; i < getPages().size(); i++) {
+                SettingsMenu menu = getPage(i);
+                Inventory inventory = menu.getInventory();
+                if(event.getClickedInventory() != null && event.getClickedInventory().equals(inventory)) {
+                    if(event.getCurrentItem().equals(getNextPageItem())) {
+                        if (i != getPages().size() - 1)
+                            getPage(i+1).sendInventory(event.getWhoClicked());
+                        else
+                            getPage(0).sendInventory(event.getWhoClicked());
+                        event.setCancelled(true);
+                        return;
+                    } else if(event.getCurrentItem().equals(getPrevPageItem())) {
+                        if(i != 0)
+                            getPage(i-1).sendInventory(event.getWhoClicked());
+                        else
+                            getPage(getPages().size()-1).sendInventory(event.getWhoClicked());
+                        event.setCancelled(true);
+                        return;
+                    }
+                }
+
+            }
+        }
+    }
+}
