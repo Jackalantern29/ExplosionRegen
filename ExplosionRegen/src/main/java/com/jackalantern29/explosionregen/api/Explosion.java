eb8c1396233bebf0b6d6f438d2370d9a4a05214f
Jackalantern29 <mikalmck29@gmail.com>
2021-04-08 14:46:58 -0400

    Remove unused import

--- ExplosionRegen/src/main/java/com/jackalantern29/explosionregen/api/Explosion.java
+++ ExplosionRegen/src/main/java/com/jackalantern29/explosionregen/api/Explosion.java
@@ -25,7 +25,6 @@ import org.bukkit.block.data.type.Door;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.*;
