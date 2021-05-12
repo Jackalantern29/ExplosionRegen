cada799699451f120926221a8aac7ee3e2e80d01
Jackalantern29 <mikalmck29@gmail.com>
2021-05-12 03:49:50 -0400

    Fix water & lava preventing blocks from regenerating in place

--- ExplosionRegen/src/main/java/com/jackalantern29/explosionregen/api/Explosion.java
+++ ExplosionRegen/src/main/java/com/jackalantern29/explosionregen/api/Explosion.java
@@ -534,7 +534,7 @@ public class Explosion {
 			state.update(true, false);
 		else
 			state.update(true);
-		if(bState.getType() != Material.AIR) {
+		if(bState.getType() != Material.AIR && bState.getType() != Material.WATER & bState.getType() != Material.LAVA) {
 			block.getBlock().breakNaturally();
 			bState.update(true);
 		}
